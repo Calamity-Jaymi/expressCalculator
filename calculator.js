74ac153581265cc0c53adc952076668fc27e87d1
jaymi <jaymiclymens@gmail.com>
2021-05-16 21:56:22 -0700

    Set Up Bare Bones Express Server

--- calculator.js
+++ calculator.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const app = express();
 
+//give the server something to display
+//first parameter is the location of the get request
+//second parameter is a callback function with request and response
+app.get("/", (req, res) => {
+    res.send('<h1>Hello, Cleaveland</h1>')
+});
+
 app.listen(3000);
 /// ^bare bones of an express server
 
